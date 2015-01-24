6e44a86f138485ec85a1352401efb3d940e1e0cf
Terry Nguyen <terreh@terrehbyte.com>
2015-01-24 10:38:30 -0800

    Sequencer in place

---
+++ Assets/TriplePeaks/PawnShopSequence.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PawnShopSequence : MonoBehaviour
+{
+    public WaypointCharacter Protagonist;
+    public DialogueSystem PawnShopDialogue;
+
+    int progress = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+        // subscribe to waypoint system
+        Protagonist.WaypointChanged += HandleWaypointCharacter;
+        Protagonist.ShouldMove = true;  // start the first move
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void HandleWaypointCharacter(object sender, WaypointArgs wayArgs)
+    {
+        if (progress == 0)
+        {
+            Protagonist.ShouldMove = false;
+        }
+
+        ++progress;
+    }
+}
