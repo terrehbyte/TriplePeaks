3202640326fa39315e31a12a0a46564e695284e1
samuelLegus <samuel.legus@live.com>
2015-01-25 00:34:42 -0800

    Cutscene done

--- Assets/TriplePeaks/Scripts/Evil Little Sister Battle/EvilSister.cs
+++ Assets/TriplePeaks/Scripts/Evil Little Sister Battle/EvilSister.cs
@@ -6,20 +6,33 @@ using System;
 public class EvilSister : MonoBehaviour 
 {
 	public GameObject _Brother;			//Our victim!
+	public GameObject _Cutscene;
 	public float _Speed = 15F;			//How fast we can chase him down
 	public int _ChaseInterval = 3;		//How often we can charge (in seconds)
 	public int _Health = 4;				//How many vegetables we can stomache before we die.
 	public bool _Invulnerable = true;	//Flag for when 
 	public Text _Dialogue;
 
-	private DateTime _lastChaseTime = DateTime.Now;
+	public  DateTime _lastChaseTime = DateTime.Now;
+
+	public DateTime LastDialogueDisplay {get;set;}
+
 	private Vector3 _positionToCharge = new Vector3(0,0,0);
 
 
 	// Use this for initialization
 	void Start () 
 	{
-		_positionToCharge = _Brother.transform.position;
+		LastDialogueDisplay = DateTime.Now;
+
+		if(_Cutscene.GetComponent<Cutscene>().Finished == true)
+		{
+			_positionToCharge = _Brother.transform.position;
+		}
+		else
+		{
+			_Dialogue.text = "";
+		}
 	}
 	
 	// Update is called once per frame
@@ -27,32 +40,41 @@ public class EvilSister : MonoBehaviour
 	{
 		if(_Brother)
 		{
-			//Evil sister will charge every x seconds, where x is the integer value set in the inspector.
-			if(DateTime.Now >= _lastChaseTime + new TimeSpan(0,0,_ChaseInterval))
+			if(_Cutscene.GetComponent<Cutscene>().Finished == true)
 			{
-				_lastChaseTime = DateTime.Now;
-				_positionToCharge = _Brother.transform.position;
-				transform.LookAt (_positionToCharge);
-			}
-			
-			//Evil sister is invulnerable to veggies unless her mouth is open!
-			//Her mouth is open when she's moving charging.
-			if(Vector3.Distance (transform.position, _positionToCharge) <= 1.5F)
-			{
-				_Invulnerable = true;
-			}
-			else
-			{
-				_Invulnerable = false;
-			}
-			
-			//Chhharrggggeee!
-			transform.position = Vector3.MoveTowards (transform.position, _positionToCharge, _Speed * Time.deltaTime);
-			
-			//THE VEGGIES OH GOD I NEED BLOOOOOODDDDDDDDDDD...
-			if(_Health <= 0)
-			{
-				Destroy (gameObject);
+				//Evil sister will charge every x seconds, where x is the integer value set in the inspector.
+				if(DateTime.Now >= _lastChaseTime + new TimeSpan(0,0,_ChaseInterval))
+				{
+					_lastChaseTime = DateTime.Now;
+					_positionToCharge = _Brother.transform.position;
+					transform.LookAt (_positionToCharge);
+				}
+				
+				//Evil sister is invulnerable to veggies unless her mouth is open!
+				//Her mouth is open when she's moving charging.
+				if(Vector3.Distance (transform.position, _positionToCharge) <= 1.5F)
+				{
+					_Invulnerable = true;
+				}
+				else
+				{
+					_Invulnerable = false;
+				}
+				
+				//Chhharrggggeee!
+				transform.position = Vector3.MoveTowards (transform.position, _positionToCharge, _Speed * Time.deltaTime);
+				
+				//THE VEGGIES OH GOD I NEED BLOOOOOODDDDDDDDDDD...
+				if(_Health <= 0)
+				{
+					Destroy (gameObject);
+				}
+
+				//Clear battle text after a second or so
+				if(DateTime.Now >= LastDialogueDisplay + new TimeSpan(0,0,1))
+				{
+					_Dialogue.text = "";
+				}
 			}
 		}
 	}
@@ -62,6 +84,7 @@ public class EvilSister : MonoBehaviour
 		//Stop if we hit our brother
 		if(col.gameObject == _Brother)
 		{
+			LastDialogueDisplay = DateTime.Now;
 			_positionToCharge = transform.position;
 			_Brother.GetComponent<Player>()._Health--;
 			_Dialogue.text = "OMM NOM NOM NOM NOM !";
--- Assets/TriplePeaks/Scripts/Evil Little Sister Battle/Player.cs
+++ Assets/TriplePeaks/Scripts/Evil Little Sister Battle/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour
 {
 		
 	public float _Speed = 6F;
+	public GameObject _Cutscene;
 	public GameObject _VeggiePrefab;
 	public GameObject _Sister;
 	private DateTime _whenLastShot;
@@ -33,42 +34,45 @@ public class Player : MonoBehaviour
 
 	void DetectInput()
 	{
-		//Shooting
-
-		if(Input.GetMouseButtonUp(0) && DateTime.Now >= _whenLastShot + _shotDelay)
+		if(_Cutscene.GetComponent<Cutscene>().Finished == true)
 		{
-			if(_Sister)
+			//Shooting
+			if(Input.GetMouseButtonUp(0) && DateTime.Now >= _whenLastShot + _shotDelay)
 			{
-				_whenLastShot = DateTime.Now;
-
-				Vector3 directionToSister = Vector3.Normalize(_Sister.transform.position - transform.position);
-				Vector3 offset = directionToSister * 1.5F;
-
-				GameObject newVeggie = Instantiate (_VeggiePrefab, transform.position + offset, transform.rotation) as GameObject;
-				newVeggie.GetComponent<Veggie>()._Target = _Sister.transform.position;
+				if(_Sister)
+				{
+					_whenLastShot = DateTime.Now;
+					
+					Vector3 directionToSister = Vector3.Normalize(_Sister.transform.position - transform.position);
+					Vector3 offset = directionToSister * 1.5F;
+					
+					GameObject newVeggie = Instantiate (_VeggiePrefab, transform.position + offset, transform.rotation) as GameObject;
+					newVeggie.GetComponent<Veggie>()._Target = _Sister.transform.position;
+				}
 			}
+			
+			//Movement
+			Vector3 movement = new Vector3(0F, 0F, 0F);
+			
+			if(Input.GetKey (KeyCode.UpArrow))
+			{
+				movement.z = _Speed;
+			}
+			if(Input.GetKey (KeyCode.DownArrow))
+			{
+				movement.z = _Speed * -1;
+			}
+			if(Input.GetKey (KeyCode.LeftArrow))
+			{	
+				movement.x = _Speed * -1;
+			}
+			if(Input.GetKey (KeyCode.RightArrow))
+			{
+				movement.x = _Speed;
+			}
+			
+			transform.Translate (movement * Time.deltaTime);
 		}
 
-		//Movement
-		Vector3 movement = new Vector3(0F, 0F, 0F);
-
-		if(Input.GetKey (KeyCode.UpArrow))
-		{
-			movement.z = _Speed;
-		}
-		if(Input.GetKey (KeyCode.DownArrow))
-		{
-			movement.z = _Speed * -1;
-		}
-		if(Input.GetKey (KeyCode.LeftArrow))
-		{	
-			movement.x = _Speed * -1;
-		}
-		if(Input.GetKey (KeyCode.RightArrow))
-		{
-			movement.x = _Speed;
-		}
-
-		transform.Translate (movement * Time.deltaTime);
 	}
 }
