d7de7a35feffbc72f52b70e6de4097337ebbb303
Ian Rich <iancarnation@gmail.com>
2015-01-24 18:49:29 -0800

    Merged dev into Ian

---
+++ Assets/TriplePeaks/Scripts/Lodge/PlayerCarControl.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCarControl : MonoBehaviour {
+
+	public Transform  playerTransform;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//currentPosition.position = playerTransform.position;
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			if (playerTransform.position.x <= 4)
+						playerTransform.Translate (new Vector3 (speed * Time.deltaTime, 0, 0));
+				}
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			if (playerTransform.position.x >= -4)
+				playerTransform.Translate (new Vector3 (-speed * Time.deltaTime, 0, 0));
+				}
+		//nextPosition.position.x = currentPosition.position.x + moveX;
+		//playerTransform.position = nextPosition.position;
+
+	}
+}
